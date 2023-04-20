e97b6a5c2f033b49885e3a2bbf5d7480ac45f29f
YoshyFlores <yoshy.floresg17@gmail.com>
2023-04-19 21:29:29 -0600

    Adding dist

---
+++ assets/IntroView-8a5b6934.js
@@ -0,0 +1,29 @@
+import{B as o}from"./InvesComponent-4a37eda4.js";import{o as r,c as s,a as e,b as a}from"./index-5291fdb0.js";const c={class:"container text-center"},d={class:"container text-center"},l=e("div",{class:"p-3 bg-info bg-opacity-10 border border-info border-start-0 rounded-end"},[e("h2",null,"Introducción")],-1),m=e("br",null,null,-1),p={class:"col-lg-6 mx-auto"},u={__name:"Introduccion",setup(n){return(t,i)=>(r(),s("div",c,[e("div",d,[l,m,e("div",p,[a(o,{text:`En el presente portafolio se brindará información de todo lo aprendido\r
+        durante el primer computo de la materia de Programación IV, en este se\r
+        mostrarán capturas y fotos de las actividades y los apuntes tomados en\r
+        clases. Este primer computo estuvo lleno de muchos retos, ya que\r
+        iniciamos a trabajar con VueJS, así mismo involucramos Vite y Bootstrap.\r
+        Al inicio considero que muchos aspectos de trabajar tanto con JavaScript\r
+        y VueJS era complicados, ya que es una estructura un poco peculiar, pero\r
+        mediante las explicación e investigaciones externas se logro entender de\r
+        cierta forma todos los elementos y contenidos que se van presentando en\r
+        las clases. Se han realizados diversas actividades, en las practicas se\r
+        ha estado trabajando en una página, en la cual se crearon componentes y\r
+        vistas respectivas para conformar la página web, así mismo se realizo\r
+        filtrado. Así mismo, se trabajó con Bootstrap, este es un marco de desarrollo web\r
+        que permite dar estilo a nuestras páginas web, anteriormente ya había\r
+        trabajado con este marco de interfaz, pero en cosas pequeñas y\r
+        sencillas, así que se volvió un reto poder incorporar directamente un\r
+        template en vue.js pero mediante la indagación y la practica se logro\r
+        trabajar con este. Durante las clases prácticas se desarrollaron muchas actividades,\r
+      creamos una pequeña página web, así mismo aprendimos como consumir una API y realizamos \r
+      filtrado. Todos los cambios y actividades que realizamos las subimos a un repositorio en Github y\r
+      posteriormente lo compartimos como evidencia que se ha trabajado en clases.\r
+      Esencialmente, lo que se ha recabado en este portafolio es una parte de todo lo visto y aprendido\r
+      en clases.`}),a(o,{text:`Por otra parte, en el computo dos, se continuo trabajando con nuestro portafolio, pero de igual forma\r
+      se desarrollaron diversos temas que se presentan en dicho trabajo, se podrá observar que hemos trabajado con \r
+      laravel así como también la utilización de mysql para la creación de algunas base de datos. Se ha trabajado con \r
+      PHP, extrayendo lo más importante de este lenguaje de programación. Básicamente, se ha plasmado la parte esencial \r
+      de todo lo realizado tanto en el primer como en el segundo computo con el objetivo de mostrar lo que se ha\r
+      aprendido y estudiado. Cabe mencionar que se comparte el enlace al repositorio de cada computo, donde se evidencian\r
+      todas las prácticas y actividades realizadas. `})])])]))}},_={__name:"IntroView",setup(n){return(t,i)=>(r(),s("main",null,[a(u)]))}};export{_ as default};
